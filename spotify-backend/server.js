2824158bb87c7aa0205d5e3bf5a4d42222c85f35
Tim Leuschner <tim.leuschner@gmail.com>
2019-05-08 08:21:38 +0200

    database and stuff

--- spotify-backend/server.js
+++ spotify-backend/server.js
@@ -1,105 +1,64 @@
 require('dotenv').config();
 let express = require('express');
-let request = require('request');
-let querystring = require('querystring');
-let cookieParser = require('cookie-parser');
+let bodyParser = require('body-parser');
+let mysql = require('mysql');
+let cors = require('cors');
 
 const app = express();
+const jsonParser = bodyParser.json();
 const PORT = process.env.PORT || 1234;
-
-/*
-
-//Authorization code flow 
-//Auskommentiert, da implicit flow genutzt wird
-
-//constants
-
-const clientId = process.env.CLIENT_ID;
-const clientSecret = process.env.CLIENT_SECRET;
-const redirectURI = process.env.REDIRECT_URI;
-const stateKey = 'auth_state';
-
-//helper functions
-let randomString = function (length) {
-    let text = '';
-    let possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-
-    for (let i = 0; i < length; i++) {
-        text += possible.charAt(Math.floor(Math.random() * possible.length));
-    }
-
-    return text;
-};
-
-//express middleware setup
-app.use(cookieParser());
-
-
-app.get('/login', (req, res) => {
-    let state = randomString(16);
-    let scope = 'user-read-private user-read-email playlist-read-private user-top-read user-read-recently-played';
-
-    res.cookie(stateKey, state);
-    res.redirect(`https://accounts.spotify.com/authorize?${querystring.stringify({
-        response_type: 'code',
-        client_id: clientId,
-        scope: scope,
-        redirect_uri: redirectURI,
-        state: state
-    })}`);
+let connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
-
-app.get('/callback', (req, res) => {
-    const code = req.query.code || null;
-    const state = req.query.state || null;
-    const storedState = req.cookies ? req.cookies[stateKey] : null;
-
-    if (state === null || state !== storedState) {
-        res.redirect(`http://localhost:4200/#${querystring.stringify({ error: 'state_error' })}`);
-    } else {
-        res.clearCookie(stateKey);
-
-        let authOptions = {
-            url: 'https://accounts.spotify.com/api/token',
-            form: {
-                code: code,
-                redirect_uri: redirectURI,
-                grant_type: 'authorization_code'
-            },
-            headers: {
-                'Authorization': 'Basic ' + (new Buffer.from(clientId + ':' + clientSecret).toString('base64'))
-            },
-            json: true
-        };
-
-        request.post(authOptions, (error, response, body) => {
-            if (!error && response.statusCode === 200) {
-                let accessToken = body.access_token,
-                    refreshToken = body.refresh_token;
-
-                    console.log(body);
-
-                //redirect to frontend
-                res.redirect(`http://localhost:4200/login?${querystring.stringify({
-                    access_token: accessToken,
-                    refresh_token: refreshToken
-                })}`);
-            } else {
-                res.redirect('http://localhost:4200/error');
-            }
-        });
+connection.connect((err) => {
+    if (err) {
+        console.error('error connecting: ' + err.stack);
+        return;
     }
 });
 
-*/
-
-
+app.use(jsonParser).use(cors());
 
 
 app.get('/', (req, res) => {
     res.write('Hello World');
     res.end();
-})
+});
+
+app.get('/visitors', (req, res) => {
+    let uniqueUsers;
+    connection.query('SELECT COUNT(*) AS visitors FROM users ', (err, result) => {
+        if (err) {
+            res.status(400).json({
+                "visitors": 000
+            });
+        } else {
+            uniqueUsers = result[0].visitors;
+            res.status(200).json({
+                "visitors": uniqueUsers
+            });
+        }
+    });
+});
+
+
+//TODO: double check status codes
+app.post('/user', (req, res) => {
+    if (req.body && req.body.spotifyID && req.body.country) {
+        let country = req.body.country;
+        let spotifyID = req.body.spotifyID;
+        connection.query('INSERT IGNORE INTO users (SPOTIFY_ID, COUNTRY) VALUES (?, ?)', [spotifyID, country], (err) => {
+            if (err) throw err;
+        });
+        res.status(200).json(null);
+
+    } else {
+        res.status(400).json(null);
+    }
+});
 
 app.listen(PORT, () => {
     console.log(`listening on Port ${PORT}`);
