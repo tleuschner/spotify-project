1e917877e32d3ce8efe406854ea175570d07a5e6
denniswillers <d.willers@gmx.net>
2019-05-09 13:16:07 +0200

    Detail mit Input von Artist füllen

--- spotify-project/src/app/artist-details/artist-details.component.ts
+++ spotify-project/src/app/artist-details/artist-details.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Artist} from "../models/SpotifyObjects";
 import {SpotifyService} from "../services/spotify.service";
+import {DetailObject} from "../models/DetailObject";
 
 @Component({
   selector: 'app-artist-details',
@@ -10,6 +11,7 @@ import {SpotifyService} from "../services/spotify.service";
 export class ArtistDetailsComponent implements OnInit {
 
   private topArtists: Artist[];
+  private artistDetails: DetailObject[] = [];
 
   constructor(private spotifyService: SpotifyService) { }
 
@@ -17,6 +19,16 @@ export class ArtistDetailsComponent implements OnInit {
     this.spotifyService.timeRange.subscribe(time => {
       this.spotifyService.getTopArtists('50', undefined, time).subscribe(res => {
         this.topArtists = res;
+        for(let artist of this.topArtists) {
+          let i = 1;
+          this.artistDetails.push({
+            image: artist.images[0].url,
+            firstLine: artist.name,
+            secondLine: i.toString(),
+            id: artist.id
+          });
+          i++;
+        }
       });
     });
   }
--- spotify-project/src/app/detail-list/detail-list.component.ts
+++ spotify-project/src/app/detail-list/detail-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {DetailObject} from "../models/DetailObject";
 
 @Component({
   selector: 'app-detail-list',
@@ -6,10 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./detail-list.component.css']
 })
 export class DetailListComponent implements OnInit {
+@Input() detailObject: DetailObject;
 
   constructor() { }
 
   ngOnInit() {
+    console.log(this.detailObject);
   }
 
 }
